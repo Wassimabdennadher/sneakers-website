147a153aef202bd0d14e426ec50c18197a823d26
Wassim Abdennadher <wassim.abd@gmail.com>
2022-12-26 21:32:20 +0100

    image changes

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const minus = document.querySelector('.minus')
 const plus = document.querySelector('.plus')
 const count = document.querySelector('#count')
+const imagesList = document.querySelectorAll('img')
+const mainimage = document.querySelector('.mainimage')
 console.log(minus)
 console.log(plus)
 console.log(count.textContent)
@@ -12,4 +14,12 @@ minus.addEventListener('click', function(event){
 plus.addEventListener('click', function(event){
     event.preventDefault()
     count.textContent++
+})
+imagesList.forEach(function(el){
+    el.addEventListener('click', function(event){
+        let attribute = el.getAttribute('src')
+        event.preventDefault()
+        if (!el.classList.contains('mainimage'))
+        {mainimage.setAttribute('src',attribute)}
+    })
 })
